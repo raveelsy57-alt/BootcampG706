c5605dc70a692a5fe1e44e2f813595a394fdf755
elsy rave <raveelsy57@gmail.com>
2026-04-27 09:30:45 -0500

    27/04/2026

--- mision2/chatv2/chatbot/model.py
+++ mision2/chatv2/chatbot/model.py
@@ -7,3 +7,45 @@ MODEL_DIR = "models"
 MODEL_PATH = os.path.join(MODEL_DIR, "chatbot_model.pkl")
 VECTORIZER_PATH = os.path.join(MODEL_DIR, "vectorizer.pkl")
 ANSWERS_PATH = os.path.join(MODEL_DIR, "answers.pkl")
+
+def buid_and_train_model(train_pairs):
+    questions = [q for q, _ in train_pairs]  
+    answers = [a for _, a in train_pairs]
+    vectorizer = CountVectorizer()
+    x = vectorizer.fit_transform(questions)
+    unique_answers = sorted(set(answers))
+    answer_to_label ={a: i for i, a in enumerate(unique_answers)}
+    y = [answer_to_label[a] for a in answers]
+    Model = MultinomialNB()
+    model. fit(x,y)
+    #crear carpeta para guardar el modelo si no existe
+    os.makedirs(MODEL_DIR,existe_ok=true)
+    #Guardar los oobjetos entrenados
+    wit open( MODEL_PATH,"wb") as f:
+       pickle.dump(model,f)
+    with open(VECTORIZER_PATH, "wb") as f:
+        pickle.dump(vectorizer, f)
+    with open(ANSWERS_PATH, "wb") as f:
+        pickle.dump(unique_answers, f)
+        print("🆗 modelo entrenado y guardado correctamente")
+        return model, vectorizer,unique_answers
+    
+def load_model():
+    if(
+        os.path.exists(MODEL_PATH)  
+        and os.path.exists(VECTORIZER_PATH) 
+        and os.path.exists(ANSWERS_PATH)
+    ):
+        with open(MODEL_PATH, "rb") as f:
+            model = pickle.load(f)
+        with open(VECTORIZER_PATH, "rb") as f:
+            vectorizer = pickle.load(f)
+        with open(ANSWERS_PATH, "rb") as f:
+            unique_answers = pickle.load(f)
+        print("📁 Modelo cargado desde disco.")
+        return model, vectorizer, unique_answers
+    else:
+        print("⚠️ no hay modelo guardado.sera necesario encontrarlo")
+        return None,None,None
+     
+          
